daa04547cb4e96574a5d37b0e50a8acaebe99d9f
Pseudoinf15 <144325080+Pseudoinf15@users.noreply.github.com>
2024-12-13 08:01:40 +0100

    Add files via upload

--- baza danych/database.sql
+++ baza danych/database.sql
@@ -5,3 +5,9 @@ CREATE TABLE wydarzenia(
     wydarzenie_data date,
     wydarzenie_czas time
 );
+CREATE TABLE logowanie( 
+    id int AUTO_INCREMENT PRIMARY KEY,
+    email varchar(255),
+    login varchar(255),
+    password varchar(255)
+);
