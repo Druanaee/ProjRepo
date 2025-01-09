24151b206fb0c6e7a97d49dcc6a7cc1be04ba380
Fl5r <144325105+minigun20050@users.noreply.github.com>
2025-01-09 09:56:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
     let btn = document.querySelectorAll("button[id='settings']");
-    let j = 0;
     btn.forEach((el) => {
         el.addEventListener('click', () => {
-            if(j==0) {
-                j = 1;
-                document.getElementById("white").style.display = "block";
-                document.getElementById("blue").style.display = "block";
-                document.getElementById("black").style.display = "block";
-            } else {
-                j = 0;
-                document.getElementById("white").style.display = "none";
-                document.getElementById("blue").style.display = "none";
-                document.getElementById("black").style.display = "none";
-            }
+            document.getElementsByClassName("set").style.display='block';
         })
     });
 });
